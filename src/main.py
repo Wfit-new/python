774408e331128abbf8ea036204adb6eee04a4d19
root <root@localhost>
2021-12-25 13:18:36 +0100

    auto publish 1640434716402

--- src/main.py
+++ src/main.py
@@ -1,22 +1,19 @@
-class StarBox:
-    def __init__(self):
-        self.stock = 100
-        self.sell = 0
-    def new_order(self, quantity):
-        if quantity < self.stock:
-            self.stock = self.stock - quantity
-            self.sell = self.sell + 3.5*quantity
-            print("thanks for buying coffee")
-        else:
-            print("Sorry! We are out of coffee")
-            self.restock()
-    def restock(self):
-        self.stock = self.stock + 100
-        print("Restocked. Updated stock", self.stock)
+class Car:
+    car_name = "Wheeljack"
+    def move(self):
+        print(self.car_name, "Car is Moving")
 
-coffeeBar = StarBox()
-coffeeBar.new_order(30)
-coffeeBar.new_order(30)
-coffeeBar.new_order(30)
-coffeeBar.new_order(30)
-coffeeBar.new_order(30)
+class Truck:
+    truck_name = "Optimus Prime"
+    def move(self):
+        print( self.truck_name, "Truck is moving")
+
+class Bike:
+    bike_name = "Elita-One"
+    def move(self):
+        print(self.bike_name, "Bike is moving")
+
+vehicles = [Car(), Truck(), Bike()]
+
+for vehicle in vehicles:
+    vehicle.move()
